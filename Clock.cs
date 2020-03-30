4612096b465d85ee65bccb49881eda8c902a45cd
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 13:47:19 +0200

    Initial commit

---
+++ Clock.cs
@@ -0,0 +1,44 @@
+﻿namespace ClockV10
+{
+    public class Clock
+    {
+        #region Instance Fields
+        private int _timeInMinutesSince0000;
+        #endregion
+
+        #region Constructor
+        public void Settime(int Hours, int Minutes)
+        {
+            _timeInMinutesSince0000 = 60 * Hours + Minutes;
+        }
+        #endregion
+
+        #region Properties
+        public int Hours
+        {
+            get {return _timeInMinutesSince0000 / 60;}
+        }
+        public int Minutes
+        {
+            get {return _timeInMinutesSince0000 % 60;}
+        }
+        
+        public int GetHours
+        {
+            get { return Hours;}
+        }
+
+        public int GetMinutes
+        {
+            get { return Minutes;}
+        }        
+        #endregion
+
+        #region Methods
+        public void AddTime()
+        {
+            GetMinutes
+        }
+        #endregion
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            Clock myClock = new Clock();
+            myClock.Settime(12, 51);
+
+            System.Console.WriteLine($"The the current time is {myClock.GetHours}:{myClock.GetMinutes}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
